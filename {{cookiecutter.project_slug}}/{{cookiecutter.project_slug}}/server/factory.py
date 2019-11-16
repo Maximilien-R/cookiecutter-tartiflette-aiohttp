72b3778bae7c26542a023d46d0c9b59c8a3ec04c
Maximilien Raulic <raulic.maximilien@gmail.com>
2019-11-16 22:24:09 +0100

    refactor(factory): transform "create_app" to an asynchronous function

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/server/factory.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/server/factory.py
@@ -11,7 +11,7 @@ from {{cookiecutter.project_slug}}.server.registries import (
 __all__ = ("create_app",)
 
 
-def create_app() -> "aiohttp.web.Application":
+async def create_app() -> "aiohttp.web.Application":
     """
     Create and setup the application to run.
     :return: the application instance to run
